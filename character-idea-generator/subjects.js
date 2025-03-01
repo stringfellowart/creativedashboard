b22ec74273d4db7e4c26d0329f157116a3b5969d
stringfellowart <stringfellowart@gmail.com>
2025-02-28 17:12:13 -0700

    Create subjects.js

---
+++ character-idea-generator/subjects.js
@@ -0,0 +1,15 @@
+{
+    "subjects": [
+        "an owl", "a deer", "a fox", "a boar", "a sable", "a witch", "forest spirit", "two sisters", "a fairy", "little people",
+        "an herbalist", "a prophet", "a stargazer", "a hermit", "a frog", "a maiden", "a jester", "an orphan", "a peddler", "a sailor",
+        "a mermaid", "a mouse", "a storyteller", "a bear", "two brothers", "three maidens", "a fox cub", "a treasure hunter",
+        "an artist", "a seamstress", "a tailor", "a blacksmith", "goddess", "a toy maker", "a map maker", "pigeon keeper",
+        "a musician", "mother", "daughter", "son", "father", "three crones", "a bookseller", "a scholar", "a groom",
+        "a leprechaun", "a scribe", "an archer", "a woodcutter", "a fisherman", "a shopkeeper", "a bride", "a knight", "druid",
+        "a farmer", "a stag", "willow spirit", "a pixie", "a bard", "a golem", "an elf", "a sprite", "a shepherd", "a raven", "a crone",
+        "a crow", "a dragon", "an elder", "an elk", "a falcon", "spirit", "a giant", "a guardian", "a hawk", "a pony", "a hound",
+        "a hunter", "a woodsman", "a king", "a lady", "a lord", "a mage", "a monk", "a monster", "a phoenix", "a prince", "a princess",
+        "a queen", "a serpent", "a spider", "a squire", "a swan", "a thief", "a toad", "a traveler", "a troll", "twins", "a vampire",
+        "a warrior", "a widow", "a wizard", "a wolf", "a wraith"
+    ]
+}
